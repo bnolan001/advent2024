ce4107788a118d591b35937c78fe6a0f127d0674
Brian Nolan <bnolan001@gmail.com>
2024-12-02 01:02:09 +0000

    Dec 1, 2024, 8:02 PM

---
+++ day01/problem2.py
@@ -0,0 +1,16 @@
+
+with open("sample.txt", "r", encoding="utf8") as file:
+    left = []
+    right = []
+    for line in file:
+        values = [int(x) for x in line.split()]
+        left.append(values[0])
+        right.append(values[1])
+
+    left.sort()
+    right.sort()
+    total = 0
+    for i in range(len(left)):
+        diff = abs(left[i] - right[i])
+        total += diff
+    print(total)
