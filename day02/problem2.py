d8d0af6835adb6151d853396e302e5cfafa89c20
Brian Nolan <bnolan001@gmail.com>
2024-12-03 03:29:48 +0000

    Dec 2, 2024, 10:29 PM

--- day02/problem2.py
+++ day02/problem2.py
@@ -1,17 +1,28 @@
+import copy 
+
+def is_safe(values):
+    reverse = values[0] > values[len(values) - 1]
+    copied_values = copy.deepcopy(values)
+    copied_values.sort(reverse=reverse)
+    if (values == copied_values):
+        safe = True
+        for i in range(len(values) - 1):
+            diff = abs(values[i] - values[i + 1])
+            if 0 == diff or diff  > 3:
+                safe = False
+                break
+        return safe
+    return False
 
 with open("data.txt", "r", encoding="utf8") as file:
-    left = []
-    right = {}
+    safe_count = 0
     for line in file:
         values = [int(x) for x in line.split()]
-        left.append(values[0])
-        if values[1] not in right:
-            right[values[1]] = 0
-        right[values[1]] += 1
+        reverse = values[0] > values[len(values) - 1]
 
-    left.sort()
-    total = 0
-    for i in range(len(left)):
-        instances = 0 if left[i] not in right else right[left[i]] * left[i]
-        total += instances
-    print(total)
+        if is_safe(values):
+            safe_count += 1
+        
+
+    print(safe_count)
+   
