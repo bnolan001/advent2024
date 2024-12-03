b39193d941c46ec0dd328a1759d7c80189bfd9ba
Brian Nolan <bnolan001@gmail.com>
2024-12-03 03:25:13 +0000

    Dec 2, 2024, 10:25 PM

--- day02/problem1.py
+++ day02/problem1.py
@@ -1,6 +1,6 @@
 import copy 
 
-with open("sample.txt", "r", encoding="utf8") as file:
+with open("data.txt", "r", encoding="utf8") as file:
     safe_count = 0
     for line in file:
         values = [int(x) for x in line.split()]
@@ -11,7 +11,8 @@ with open("sample.txt", "r", encoding="utf8") as file:
         if (values == copied_values):
             safe = True
             for i in range(len(values) - 1):
-                if 0 == abs(values[i] - values[i + 1]) > 3:
+                diff = abs(values[i] - values[i + 1])
+                if 0 == diff or diff  > 3:
                     safe = False
                     break
             if safe:
