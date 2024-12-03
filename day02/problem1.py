92b8ed53d9c6c77213d4af01b28ab100f3c8456b
Brian Nolan <bnolan001@gmail.com>
2024-12-03 03:19:48 +0000

    Dec 2, 2024, 10:19 PM

--- day02/problem1.py
+++ day02/problem1.py
@@ -4,13 +4,16 @@ with open("sample.txt", "r", encoding="utf8") as file:
     safe_count = 0
     for line in file:
         values = [int(x) for x in line.split()]
+        reverse = values[0] > values[len(values) - 1]
+
         copied_values = copy.deepcopy(values)
-        copied_values.sort()
+        copied_values.sort(reverse=reverse)
         print(values, "|", copied_values)
         if (values == copied_values):
             for i in range(len(values) - 1):
                 if 1 >= abs(values[i] - values[i + 1]) <= 3:
                     safe_count += 1
+                    break
 
     print(safe_count)
    
