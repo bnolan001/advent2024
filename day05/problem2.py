d60dde7fc8508239fef9bea76197d2ee301170e4
Brian Nolan <bnolan@gobrio.com>
2024-12-05 07:39:33 -0500

    Day 5 solution

--- day05/problem2.py
+++ day05/problem2.py
@@ -1,7 +1,37 @@
 
 
-with open("sample.txt", "r", encoding="utf8") as file:
+with open("data.txt", "r", encoding="utf8") as file:
     total = 0
-        
+    rules = {}
+    incorrect_orderings = []
+    for line in file:
+        update_manual = []
+        if "|" in line:
+            first_page, second_page = line.strip().split("|")
+            if first_page not in rules:
+                rules[first_page] = []
+            rules[first_page].append(second_page)
+
+        elif "," in line:
+            pages = line.strip().split(",")
+            
+            for page in pages:
+                if page not in rules:
+                    update_manual.append(page)
+                else:
+                    insert_index = len(update_manual)
+                    for i in range(len(rules[page])):
+                        if rules[page][i] in update_manual:
+                            before_index = update_manual.index(rules[page][i])
+                            if (before_index < insert_index):
+                                insert_index = before_index
+
+                    update_manual.insert(insert_index, page)
+            if update_manual != pages:
+                incorrect_orderings.append(update_manual)
+            print(update_manual)
+    for pages in incorrect_orderings:
+        index = len(pages) // 2
+        total += int(pages[index])
     print(total)
    
