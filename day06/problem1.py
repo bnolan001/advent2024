f77dd9c2e47b4471df3cd6cbaa8299161f90a951
Brian Nolan <bnolan001@gmail.com>
2024-12-10 01:43:50 +0000

    Dec 9, 2024, 8:43 PM

--- day06/problem1.py
+++ day06/problem1.py
@@ -9,7 +9,7 @@ with open("day06/sample.txt", "r", encoding="utf8") as file:
     total = 0
     
     for line in file:
-        map += [line.strip().split()]
+        map += [new List(line.strip())]
 
 print_map()
 print(total)
