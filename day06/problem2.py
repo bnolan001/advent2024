e9c3ca70d960d8a180fdf856333d8b14e311d89a
Brian Nolan <bnolan001@gmail.com>
2024-12-24 02:38:39 +0000

    Dec 23, 2024, 9:38 PM

--- day06/problem2.py
+++ day06/problem2.py
@@ -59,7 +59,7 @@ with open("day06/sample.txt", "r", encoding="utf8") as file:
         if next_x == -1 and next_y == -1:
             continue
 
-        if (prev_direction != next_direction) or (map[next_y][next_x] == movement_config[next_direction]['marker']):
+        if (prev_direction != next_direction) or (map[next_y][next_x] != movement_config[next_direction]['marker']):
             map[next_y][next_x] = movement_config[next_direction]['marker']
             map[prev_y][prev_x] = '+'
         elif (map[next_y][next_x] == '.'):
