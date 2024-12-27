4d09de7091820452c14760156c5e7dca4a9c5b0a
Brian Nolan <bnolan001@gmail.com>
2024-12-27 01:30:47 +0000

    Dec 26, 2024, 8:30 PM

--- day06/problem2.py
+++ day06/problem2.py
@@ -58,7 +58,7 @@ def can_loop_brute_force(x, y, direction):
         if (current_direction != next_direction):
             num_turns += 1
             current_direction = next_direction
-        path += (next_x, next_y)
+        path.add((next_x, next_y))
         (next_x, next_y, next_direction) = get_next_move(next_x, next_y, next_direction)
 
     return False
@@ -86,7 +86,7 @@ with open("day06/sample.txt", "r", encoding="utf8") as file:
     while next_x != -1 and next_y != -1:
         if (can_loop_brute_force(next_x, next_y, next_direction)):
             (block_x, block_y, block_direction) = get_next_move(next_x, next_y, next_direction)
-            blockages += (block_x, block_y)
+            blockages.add((block_x, block_y))
             print_map()
 
         (prev_x, prev_y, prev_direction) = (next_x, next_y, next_direction)
