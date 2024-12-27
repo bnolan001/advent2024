9d71830173e5583abf5b2e8c1d833869c36eb42a
Brian Nolan <bnolan001@gmail.com>
2024-12-27 01:00:30 +0000

    Dec 26, 2024, 8:00 PM

--- day06/problem2.py
+++ day06/problem2.py
@@ -56,7 +56,7 @@ def can_loop_brute_force(x, y, direction):
     num_turns = 0
     check_x = x + movement_config[direction]['move_x']
     check_y = y + movement_config[direction]['move_y']
-    if (check_x < 0 or check_y < 0  or check_y > len(map) or check_x > len(map[check_y]) or map[y + movement_config[direction]['move_y']][x + movement_config[direction]['move_x']] not in ['.', '#']):
+    if (check_x < 0 or check_y < 0  or check_y >= len(map) or check_x > len(map[check_y]) or map[y + movement_config[direction]['move_y']][x + movement_config[direction]['move_x']] not in ['.', '#']):
         return False
     
     # simulate a blockage placed on the next move
