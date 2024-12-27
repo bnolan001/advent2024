199e6e4af0401e6ceff4a35c6830e4c7d5bb6c3f
Brian Nolan <bnolan001@gmail.com>
2024-12-27 02:31:35 +0000

    Dec 26, 2024, 9:31 PM

--- day06/problem2.py
+++ day06/problem2.py
@@ -56,7 +56,8 @@ def can_loop_brute_force(x, y, direction):
     while(next_x != -1 and next_y != -1):
         steps += 1
         # if we've reached our starting point or traversed too many steps, we've looped
-        if (next_x == x and next_y == y) or ((next_x, next_y) in visited):
+        #if (next_x == x and next_y == y) or ((next_x, next_y) in visited):
+        if ((next_x, next_y) in visited):
             return True
         
         visited.add((next_x, next_y))
