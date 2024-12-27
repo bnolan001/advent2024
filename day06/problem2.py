7dddfa9c6b7e672e3f85788a126ae2739da1fc23
Brian Nolan <bnolan001@gmail.com>
2024-12-27 02:42:12 +0000

    Dec 26, 2024, 9:42 PM

--- day06/problem2.py
+++ day06/problem2.py
@@ -98,7 +98,7 @@ with open("day06/data.txt", "r", encoding="utf8") as file:
     blockages = ()
     while next_x != -1 and next_y != -1:
         if (can_loop_brute_force(next_x, next_y, next_direction)):
-            (block_x, block_y, block_direction) = get_next_move(next_x, next_y, next_direction)
+            (block_x, block_y) = (next_x + movement_config[direction]['move_x'], next_y + movement_config[direction]['move_y'])
             if (map[block_y][block_x] not in ['^', '#']):
                 blockages += ((block_x, block_y), )
             #print_map()
