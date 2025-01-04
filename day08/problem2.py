66600e444fb367b35dda6fe48c48c6bc731ca009
Brian Nolan <bnolan001@gmail.com>
2025-01-04 02:36:27 +0000

    Jan 3, 2025, 9:36 PM

--- day08/problem2.py
+++ day08/problem2.py
@@ -7,25 +7,6 @@ def print_map(map):
     for line in map:
         print("".join(line))
 
-def mark_antinodes_on_line(i, j, node, main_map):
-    new_nodes = 0
-    for k in range(j+1, len(main_map[i])):
-        if main_map[i][k] == node:
-            x_axis = k - j
-            ant_j = j - x_axis
-            while ant_j >= 0:
-                new_nodes += 1
-                antinodes[i][ant_j] = node
-                merged_nodes[i][ant_j] = node
-                ant_j = ant_j - x_axis
-            ant_j = k + x_axis
-            while ant_j < len(main_map[i]):
-                new_nodes += 1
-                antinodes[i][ant_j] = node
-                merged_nodes[i][ant_j] = node
-                ant_j = ant_j + x_axis
-    return new_nodes
-
 def mark_antinodes_vertically(y, x, node, main_map):
     for k in range(y+1, len(main_map)):
         for l in range(len(main_map[k])):
@@ -55,9 +36,6 @@ def find_antinodes(main_map):
         for x in range(len(main_map[y])):
             if main_map[y][x] != ".":
                 node = main_map[y][x]
-                # check remaining line for the same node
-                #mark_antinodes_on_line(y, x, node, main_map)
-                # check for nodes down the scan
                 mark_antinodes_vertically(y, x, node, main_map)
     
 
@@ -77,36 +55,7 @@ with open("day08/data.txt", "r", encoding="utf8") as file:
         antinodes.append(['.']*len(scan[0]))
         merged_nodes.append(list(line.strip()))
 
-    print("------Original Scan------")
-    print_map(scan)
     find_antinodes(scan)
-    print("------Post Find Antinodes------")
-    print_map(antinodes)
-    total = get_antinodes_count()
-    print("------Antinodes Count------")
+    total = get_antinodes_count()   
     print(total)
-    print()
-    # print("------Post Find Merged second------")
-    # print_map(merged_nodes)
-    
-    # print()
-    # total = find_antinodes(merged_nodes)
-    # print("------Post Find Antinodes second run------")
-    # print_map(antinodes)
-    # print("------Post Find Merged second run------")
-    # print_map(merged_nodes)
-    # print()
-    # print("------Antinodes Count second run------")
-    # print(total)
-    # print("------Post Find Nodes------")
-
-    # print()
-    # print("------Original------")
-    # print_map(scan)
-    # print()
-    # print("------Antinodes------")
-    # print_map(antinodes)
-    # print()
-    # print("------Merged Notes------")
-    # print_map(merged_nodes)
 
