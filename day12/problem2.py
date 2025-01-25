a9ec01c019f3c0d21c7063444cd225a3a126ec6c
Brian Nolan <bnolan001@gmail.com>
2025-01-25 22:08:23 +0000

    Jan 25, 2025, 5:08 PM

--- day12/problem2.py
+++ day12/problem2.py
@@ -195,7 +195,7 @@ def calculate_pricing(data):
                     
     return plant_data
 
-with open("day12/sample_4.txt", "r", encoding="utf8") as file:
+with open("day12/sample_6.txt", "r", encoding="utf8") as file:
     data = []
     distinct_plants = {"*"}
     total = 0
