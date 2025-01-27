5b43ecdc70638b86c07a329a20fbf9abbc3a6a8f
Brian Nolan <bnolan001@gmail.com>
2025-01-27 23:22:45 +0000

    Jan 27, 2025, 6:22 PM

--- day13/problem1.py
+++ day13/problem1.py
@@ -3,8 +3,22 @@ with open("day13/sample.txt", "r", encoding="utf8") as file:
     data = []
     total = 0
     for line in file:
-        data = [int(x) for x in line.strip().split()]
-
+        splitLine = line.split()
+        game = {}
+        if "A" in splitLine[1]:
+            x = int(splitLine[2].split('+')[1].replace(',', ''))
+            y = int(splitLine[3].split('+')[1])
+            game["A"] = {"X": x, "Y": y}
+        elif "B" in splitLine[1]:
+            x = int(splitLine[2].split('+')[1].replace(',', ''))
+            y = int(splitLine[3].split('+')[1])
+            game["B"] = {"X": x, "Y": y}
+        elif "Prize" in splitLine[0]:
+            x = int(splitLine[1].split('=')[1].replace(',', ''))
+            y = int(splitLine[2].split('=')[1])
+            game["Prize"] = {"X": x, "Y": y}
+        data.append(game)
+    print(data)
     
 
     print(total)
