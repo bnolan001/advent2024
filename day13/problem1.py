ad4ca5329bc23affb20b8389100989f8cfe63ad3
Brian Nolan <bnolan001@gmail.com>
2025-01-27 23:32:54 +0000

    Jan 27, 2025, 6:32 PM

--- day13/problem1.py
+++ day13/problem1.py
@@ -1,4 +1,20 @@
 
+def calculate_game_play(game):
+    totalPlays = game["Prize"]["X"] + game["Prize"]["Y"]
+    for a in range(0, game["Prize"]["X"] // game["A"]["X"]):
+        for b in range(0, game["Prize"]["Y"] // game["B"]["X"]):
+            xMovement = a * game["A"]["X"] + b * game["B"]["X"]
+            yMovement = a * game["A"]["Y"] + b * game["B"]["Y"]
+            numberOfPlays = game["Prize"]["Y"] / yMovement
+            if (game["Prize"]["X"] % xMovement and
+                game["Prize"]["Y"] % yMovement == 0 and
+                game["Prize"]["X"] // xMovement ==
+                game["Prize"]["Y"] // yMovement):
+                if (numberOfPlays < totalPlays):
+                    totalPlays = numberOfPlays
+            totalPlays += 1
+        
+
 with open("day13/sample.txt", "r", encoding="utf8") as file:
     data = []
     total = 0
