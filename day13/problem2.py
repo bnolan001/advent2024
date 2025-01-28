29e323cde830601885c109ffed71bcce3b4ca211
Brian Nolan <bnolan001@gmail.com>
2025-01-28 01:20:04 +0000

    Jan 27, 2025, 8:20 PM

--- day13/problem2.py
+++ day13/problem2.py
@@ -7,7 +7,7 @@ def calculate_game_button_presses(game):
     for a in range(0, game["Prize"]["X"] // minX + 1):
         endingYValue = game["Prize"]["Y"] // minY - 1
         if a != 0:
-            endingYValue = (game["Prize"]["Y"] - a * game["A"]["Y"])// minY - 1
+            endingYValue = (game["Prize"]["Y"] - a * game["A"]["Y"])// minY + 1
         for b in range(0, endingYValue):
             if (a * 3 + b * 1 > minCost):
                 continue
@@ -51,8 +51,8 @@ with open("day13/data.txt", "r", encoding="utf8") as file:
             y = int(splitLine[3].split('+')[1])
             game["B"] = {"X": x, "Y": y}
         elif "Prize" in splitLine[0]:
-            x = int(splitLine[1].split('=')[1].replace(',', '')) #+ 10000000000000
-            y = int(splitLine[2].split('=')[1]) #+ 10000000000000
+            x = int(splitLine[1].split('=')[1].replace(',', '')) + 10000000000000
+            y = int(splitLine[2].split('=')[1]) + 10000000000000
             game["Prize"] = {"X": x, "Y": y}
             data.append(game)
         
