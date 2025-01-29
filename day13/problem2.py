dd3f5b73adaf4a887f0523c26aa16e262ff77cc7
Brian Nolan <bnolan001@gmail.com>
2025-01-29 21:56:47 +0000

    Jan 29, 2025, 4:56 PM

--- day13/problem2.py
+++ day13/problem2.py
@@ -3,25 +3,20 @@ def calculate_game_button_presses(game):
     buttonPresses = {"A": 0, "B": 0}
     minCost = 100000000000000
     minX = min(game["A"]["X"], game["B"]["X"])
-    minY = min(game["A"]["Y"], game["B"]["Y"])
     for a in range(0, game["Prize"]["X"] // minX + 1):
-        endingYValue = game["Prize"]["Y"] // minY - 1
-        if a != 0:
-            endingYValue = (game["Prize"]["Y"] - a * game["A"]["Y"])// minY + 1
-        for b in range(0, endingYValue):
-            if (a * 3 + b * 1 > minCost):
-                continue
-
-            xMovement = a * game["A"]["X"] + b * game["B"]["X"]
-            yMovement = a * game["A"]["Y"] + b * game["B"]["Y"]
-            playCost = a * 3 + b * 1
-            if (game["Prize"]["X"] == xMovement and
-                game["Prize"]["Y"] == yMovement):
-                if (playCost < minCost):
-                    buttonPresses["A"] = a
-                    buttonPresses["B"] = b
-                    minCost = playCost
-                    print("Found game play with A:", a, "B:", b, "Cost:", playCost)
+        remainingX = game["Prize"]["X"] - game["A"]["X"] * a
+        remainingY = game["Prize"]["Y"] - game["A"]["Y"] * a
+        if (remainingX % game["B"]["X"] != 0 or remainingY % game["B"]["Y"] != 0 or 
+            remainingX // game["B"]["X"] != remainingY // game["B"]["Y"]):
+            continue
+       
+        b = remainingY // game["B"]["Y"]
+        playCost = a * 3 + b * 1
+        if (playCost < minCost):
+            buttonPresses["A"] = a
+            buttonPresses["B"] = b
+            minCost = playCost
+            print("Found game play with A:", a, "B:", b, "Cost:", playCost)
     return buttonPresses
 
 def process_games(data):
@@ -32,7 +27,7 @@ def process_games(data):
 
     return totalPlays
 
-with open("day13/data.txt", "r", encoding="utf8") as file:
+with open("day13/sample.txt", "r", encoding="utf8") as file:
     data = []
     total = 0
     game = {}
