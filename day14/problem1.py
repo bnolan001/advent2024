5aba859c6e7e32db7de98183b47153285b435d2a
Brian Nolan <bnolan001@gmail.com>
2025-02-09 18:01:21 +0000

    Feb 9, 2025, 1:01 PM

--- day14/problem1.py
+++ day14/problem1.py
@@ -1,8 +1,28 @@
+def print_robots_map(robots, dimensions):
+    map = [['.' for x in range(dimensions['width'])] for y in range(dimensions['height'])]
+    for robot in robots:
+        map[robot['y']][robot['x']] = '#'
+    for row in map:
+        print(''.join(row))
+
+def calculate_movement(robots, dimensions, timeperiod):
+    for robot in robots:
+        totalX = robot['x'] + robot['vx'] * timeperiod
+        totalY = robot['y'] + robot['vy'] * timeperiod
+        remainderX = totalX % dimensions['width']
+        remainderY = totalY % dimensions['height']
+        lapsX = totalX // dimensions['width']
+        lapsY = totalY // dimensions['height']
+        robot['x'] = remainderX
+        robot['y'] = remainderY
+    return robots
 
 with open("day14/sample.txt", "r", encoding="utf8") as file:
     robots = []
     total = 0
-    dimensions = {"width": 101, "height": 103}
+    # dimensions = {"width": 101, "height": 103} # data.txt
+    dimensions = {"width": 11, "height": 7} # sample.txt
+
     for line in file:
         splitLine = line.replace('p=', '').replace('v=', '').split(' ')
         startingPoint = splitLine[0].split(',')
@@ -13,6 +33,8 @@ with open("day14/sample.txt", "r", encoding="utf8") as file:
             'vx': int(velocity[0]),
             'vy': int(velocity[1])
         })
-        
-
+    print_robots_map(robots, dimensions)    
+    print('-------------------')
+    calculate_movement(robots, dimensions, 100)
+    print_robots_map(robots, dimensions)
     print(total) 
